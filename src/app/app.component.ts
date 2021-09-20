fb8604a250dc995c8642efdb1f1fc7cb6ca5dd77
lteruel10 <lteruel10@gmail.com>
2021-09-20 16:35:51 -0600

    39. Tarea con el contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,18 @@ export class AppComponent {
   // title = 'Contador App';
   titulo:string = 'Contador App';
   valor:number = 10;
+
+  // sumar(){
+  //   this.valor+=1;
+  // }
+  //   restar(){
+  //   this.valor-=1;
+  // }
+  public base:number=5;
+  acumular(numero:number){
+    this.valor+=numero;
+    /// base:number =5;
+
+  }
 }
+
